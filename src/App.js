7c9ef5f641a16cf946aa5f9d817d523775049dee
taibislamdipu <mailtaibislam@gmail.com>
2021-03-02 02:05:53 +0600

    loading palyer from API done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import Player from './components/Player/Player';
+import logo from './images/dream11-logo.png'
 
 function App() {
 
   const [players, setPlayers] = useState([]);
 
   useEffect(() => {
-    fetch('https://randomuser.me/api/?results=10')
+    fetch('https://randomuser.me/api/?results=12')
       .then(res => res.json())
       .then(data => console.log(setPlayers(data.results)))
   }, [])
@@ -17,25 +18,29 @@ function App() {
   return (
     <div className="App">
 
-      <header className="header-container sticky-top">
-        <div className="bg-dark">
-          <h1 className="text-white text-center py-3">Dream 11</h1>
+      <header className="header-container sticky-top bg-white">
+        <div className="bg-light py-5">
+          <img src={logo} alt="" className="img-fluid" style={{ width: '200px' }} />
         </div>
 
         <div className="text-center">
-          <h1 className="text-white">No. Of Players: {players.length}</h1>
+          <h1 className="py-3">No. Of Players: {players.length}</h1>
         </div>
       </header>
 
-      <div className="container row">
+      <div className="container-fluid bg-warning">
+        <div className="row">
         {
           players.map(player => <Player
+
             player={player}
             name={player.name.first}
             image={player.picture.large}
+
           >
           </Player>)
         }
+        </div>
       </div>
     </div>
   );
